2fe2f9d617be0362c375f8ec5c0eb6e0a538467f
Jenny Ferries <jeferrie@microsoft.com>
2020-11-20 18:44:22 -0800

    update to 4.23

--- up-console/PublicAppUsingUsernamePassword.cs
+++ up-console/PublicAppUsingUsernamePassword.cs
@@ -1,26 +1,5 @@
-﻿/*
- The MIT License (MIT)
-
-Copyright (c) 2015 Microsoft Corporation
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
 using Microsoft.Identity.Client;
 using System;
@@ -192,8 +171,6 @@ namespace up_console
                 // Mitigation: in the Azure portal, edit the manifest for your application and set the `allowPublicClient` to `true` 
                 // ------------------------------------------------------------------------
             }
-
-
             catch (MsalClientException ex) when (ex.ErrorCode == "unknown_user_type")
             {
                 // Message = "Unsupported User Type 'Unknown'. Please see https://aka.ms/msal-net-up"
